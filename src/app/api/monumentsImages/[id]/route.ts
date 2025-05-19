57b8287aad753b56067df5bb0e5994af3a2d2156
unknown <danil.perfiliv@gmail.com>
2025-05-19 18:51:37 +0700

    fixes: params promise async

--- src/app/api/monumentsImages/[id]/route.ts
+++ src/app/api/monumentsImages/[id]/route.ts
@@ -5,8 +5,8 @@ import { prisma } from "../../../../../lib/prisma";
 export const dynamic = "force-dynamic";
 
 export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     //const { id } = await Promise.resolve(params);
