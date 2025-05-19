d2f3b7fc582a3a4854e5bc9d3f3319e7e0c3ddea
unknown <danil.perfiliv@gmail.com>
2025-05-19 18:20:29 +0700

    fixes: NextRequest

--- src/app/api/monuments/[id]/route.ts
+++ src/app/api/monuments/[id]/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { prisma } from "../../../../../lib/prisma";
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
--- src/app/api/monumentsImages/[id]/route.ts
+++ src/app/api/monumentsImages/[id]/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { prisma } from "../../../../../lib/prisma";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   try {
