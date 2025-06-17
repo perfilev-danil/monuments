85cf489ff97170c0876babc5f458ece4ddcea6c7
unknown <danil.perfiliv@gmail.com>
2025-06-18 00:13:58 +0700

    SSR

--- src/app/components/CardsMazeServer.tsx
+++ src/app/components/CardsMazeServer.tsx
@@ -1,7 +1,9 @@
 import CardsMaze from "./CardsMaze";
 
-export default async function getStaticProps() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`);
+export default async function CardsMazeServer() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`, {
+    next: { revalidate: 60 },
+  });
 
   const monuments = await res.json();
 
