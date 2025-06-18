f5752965711d6e0af3e3cc987e882c0f9e851e3e
unknown <danil.perfiliv@gmail.com>
2025-06-19 03:26:40 +0700

    fixes: API_URL

--- lib/FilteredMonumentsServer.ts
+++ lib/FilteredMonumentsServer.ts
@@ -2,7 +2,7 @@
 
 export async function FilteredMonumentsServer(params: URLSearchParams) {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_URL}/api/monuments?${params.toString()}`
+    `${process.env.API_URL}/api/monuments?${params.toString()}`
   );
 
   if (!res.ok) {
