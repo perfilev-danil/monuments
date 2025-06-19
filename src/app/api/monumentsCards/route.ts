68403e2c6eccd9506c5523347b39d2c124dcd32f
unknown <danil.perfiliv@gmail.com>
2025-06-19 21:28:00 +0700

    cache test

--- src/app/api/monumentsCards/route.ts
+++ src/app/api/monumentsCards/route.ts
@@ -1,7 +1,16 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
 
+const cache = new Map<string, { data: any; expiresAt: number }>();
+const CACHE_TTL_MS = 1000 * 60 * 60 * 24; // 24 часа
+
 export async function GET() {
+  const cacheKey = "monuments";
+  const now = Date.now();
+  const cached = cache.get(cacheKey);
+  if (cached && cached.expiresAt > now) {
+    return NextResponse.json(cached.data);
+  }
   try {
     const monuments = await prisma.e24_Monument.findMany({
       include: {
@@ -9,6 +18,11 @@ export async function GET() {
         year: true,
         images: true,
       },
+    });
+
+    cache.set(cacheKey, {
+      data: monuments,
+      expiresAt: now + CACHE_TTL_MS,
     });
 
     return NextResponse.json(monuments, {
