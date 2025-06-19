3e73b15e83f8c126c271848fd186938b2ec71a69
unknown <danil.perfiliv@gmail.com>
2025-06-19 16:29:37 +0700

    cardsmaze

--- src/app/components/CardsMaze.tsx
+++ src/app/components/CardsMaze.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 export default function CardsMaze({ children }: { children: React.ReactNode }) {
   const [monuments, setMonuments] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isDesktop, setIsDesktop] = useState(false);
   const scrollContainerRef = useRef<HTMLDivElement>(null);
   const [showHint, setShowHint] = useState(false);
@@ -23,7 +23,6 @@ export default function CardsMaze({ children }: { children: React.ReactNode }) {
     return () => window.removeEventListener("resize", checkScreenSize);
   }, []);
 
-  /*
   useEffect(() => {
     const fetchMonuments = async () => {
       try {
@@ -41,7 +40,6 @@ export default function CardsMaze({ children }: { children: React.ReactNode }) {
     };
     fetchMonuments();
   }, []);
-  */
 
   useEffect(() => {
     if (!isDesktop) return;
@@ -153,7 +151,55 @@ export default function CardsMaze({ children }: { children: React.ReactNode }) {
         ref={scrollContainerRef}
         className="overflow-x-auto no-scrollbar h-full w-full flex items-center gap-4 lg:gap-8 snap-x snap-mandatory cursor-grab scroll-smooth"
       >
-        {children}
+        {!isLoading &&
+          monuments?.map((monument: any) => (
+            <div
+              key={monument.id}
+              className="h-full snap-end w-full lg:w-[calc(25%-24px)] shrink-0"
+            >
+              <div
+                className="relative overflow-hidden h-full select-none"
+                style={{ border: "1px solid black" }}
+              >
+                <Image
+                  src={
+                    monument?.images.length === 0
+                      ? `/images/contents/noimage.jpg`
+                      : `/api/images/${monument?.images[0]?.id}`
+                  }
+                  alt=""
+                  fill
+                  sizes="(max-width: 300px) 40vw, (max-width: 1200px) 60vw, 50vw"
+                  draggable={false}
+                  className="object-cover object-top hover:scale-110 transition-transform duration-300"
+                />
+                <div
+                  className="absolute bg-white bottom-0 left-0 right-0 w-full flex justify-between p-2"
+                  style={{ border: "1px solid black" }}
+                >
+                  <div className="text-center p-2 truncate">
+                    {monument?.appellation_monument?.value} (
+                    {monument?.year?.value} г. )
+                  </div>
+
+                  <Link
+                    href={`/monuments/${monument?.id}`}
+                    className="relative w-10 h-10 shrink-0 rounded-full cursor-pointer hover:scale-110 transition-transform duration-300"
+                    style={{ border: "1px solid black" }}
+                  >
+                    <Image
+                      src="/images/icons/arrow-b.png"
+                      alt=""
+                      className="p-2"
+                      fill
+                      sizes="(max-width: 768px) 60vw"
+                      style={{ transform: "rotate(180deg)" }}
+                    />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
