42bf44c61e5b9478c94c2612f6fc0ff89d255042
unknown <danil.perfiliv@gmail.com>
2025-06-21 22:20:23 +0700

    monuments arr test

--- src/app/components/CardsMazeServer.tsx
+++ src/app/components/CardsMazeServer.tsx
@@ -3,6 +3,6 @@ import { getMonuments } from "../../../lib/getMonuments";
 
 export default async function CardsMazeServer() {
   const monuments = await getMonuments();
-
+  console.log(monuments);
   return <CardsMaze monuments={monuments} />;
 }
