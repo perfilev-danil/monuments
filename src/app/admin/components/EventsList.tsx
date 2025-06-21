65f170151e080a60c2b3b9020bbf0290d5b28f1f
unknown <danil.perfiliv@gmail.com>
2025-06-21 22:18:43 +0700

    fixes: admin panel

--- src/app/admin/components/EventsList.tsx
+++ src/app/admin/components/EventsList.tsx
@@ -135,7 +135,7 @@ export default function EventsList() {
       {/* Модальное окно */}
       {showFormModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white rounded-lg p-6 max-w-lg w-full relative">
+          <div className="bg-white p-6 max-w-lg w-full relative">
             <button
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-800 text-xl"
               onClick={() => setShowFormModal(false)}
