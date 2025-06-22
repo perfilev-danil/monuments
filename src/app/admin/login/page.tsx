bee123a20bcc81c4dc82b31e62c8c35d584b865b
unknown <danil.perfiliv@gmail.com>
2025-06-22 18:49:48 +0700

    fixes: login page

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -39,8 +39,8 @@ export default function AdminLogin() {
   };
 
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+    <div className="h-screen flex items-center justify-center bg-[var(--dark)]">
+      <div className="bg-white p-8 shadow-md w-full max-w-md">
         <h1 className="text-2xl mb-6 text-center font-american">
           Панель администратора
         </h1>
