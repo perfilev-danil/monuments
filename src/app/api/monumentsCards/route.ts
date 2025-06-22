770db5f3603fd58e1e5e09cffe173413936ffecf
unknown <danil.perfiliv@gmail.com>
2025-06-22 14:49:51 +0700

    meta data

--- src/app/api/monumentsCards/route.ts
+++ src/app/api/monumentsCards/route.ts
@@ -6,36 +6,3 @@ export async function GET() {
 
   return NextResponse.json(data);
 }
-
-/*
-import { NextResponse } from "next/server";
-import { prisma } from "../../../../lib/prisma";
-
-export async function GET() {
-  try {
-    const monuments = await prisma.e24_Monument.findMany({
-      include: {
-        appellation_monument: true,
-        year: true,
-        images: {
-          take: 1,
-          orderBy: {
-            id: "asc",
-          },
-        },
-      },
-    });
-
-    return NextResponse.json(monuments, {
-      status: 200,
-    });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Ошибка при получении памятников" },
-      { status: 500 }
-    );
-  }
-}
-
-*/
