a569fafcff4f1874ffa782d8ab11fd7fd62c92ee
unknown <danil.perfiliv@gmail.com>
2025-06-23 20:54:55 +0700

    fixes: ssr&fetch error

--- src/app/components/CardsMazeServer.tsx
+++ src/app/components/CardsMazeServer.tsx
@@ -1,27 +1,35 @@
 import CardsMaze from "./CardsMaze";
 
-import { getMonuments } from "../../../lib/getMonuments";
-
 export default async function CardsMazeServer() {
-  /*
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`);
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`
+    );
 
-  if (!res.ok) {
-    throw new Error(`Ошибка HTTP: ${res.status}`);
-  }
-  */
+    if (!res.ok) {
+      return (
+        <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
+          Ошибка загрузки данных: {res.status}
+        </div>
+      );
+    }
 
-  const monumentsRaw = await getMonuments();
-  const monuments = JSON.parse(JSON.stringify(monumentsRaw));
+    const monuments = await res.json();
 
-  //const monuments = await res.json();
-  if (!monuments || monuments.length === 0) {
+    if (!monuments || monuments.length === 0) {
+      return (
+        <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
+          Нет данных о памятниках!
+        </div>
+      );
+    }
+
+    return <CardsMaze monuments={monuments} />;
+  } catch (error) {
     return (
       <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
-        Нет данных о памятниках!
+        Не удалось загрузить данные о памятниках.
       </div>
     );
   }
-  <pre>{JSON.stringify(monuments, null, 2)}</pre>;
-  return <CardsMaze monuments={monuments} />;
 }
