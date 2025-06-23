5b60b1458aac0b16955ee8c26dbd20c4b62e23c2
unknown <danil.perfiliv@gmail.com>
2025-06-23 23:04:30 +0700

    GSR

--- src/app/components/CardsMazeServer.tsx
+++ src/app/components/CardsMazeServer.tsx
@@ -1,20 +1,36 @@
 import CardsMaze from "./CardsMaze";
 
 export default async function CardsMazeServer() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`);
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`
+    );
 
-  if (!res.ok) {
-    throw new Error(`Ошибка HTTP: ${res.status}`);
-  }
+    if (!res.ok) {
+      console.error(`Ошибка HTTP: ${res.status}`);
+      return (
+        <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
+          Временные проблемы с загрузкой данных. Пожалуйста, попробуйте позже.
+        </div>
+      );
+    }
 
-  const monuments = await res.json();
-  if (!monuments || monuments.length === 0) {
+    const monuments = await res.json();
+    if (!monuments || monuments.length === 0) {
+      return (
+        <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
+          Нет данных о памятниках!
+        </div>
+      );
+    }
+
+    return <CardsMaze monuments={monuments} />;
+  } catch (error) {
+    console.error("Ошибка при загрузке данных:", error);
     return (
       <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
-        Нет данных о памятниках!
+        Произошла ошибка при загрузке данных. Пожалуйста, обновите страницу.
       </div>
     );
   }
-
-  return <CardsMaze monuments={monuments} />;
 }
