3b5c47e9417577b1bf2d5ca89b32cc5f0267d27b
unknown <danil.perfiliv@gmail.com>
2025-06-24 01:39:04 +0700

    fixes: cards maze

--- src/app/components/CardsMazeServer.tsx
+++ src/app/components/CardsMazeServer.tsx
@@ -1,13 +1,16 @@
 import CardsMaze from "./CardsMaze";
-import { getMonuments } from "../../../lib/getMonuments";
+
+export const dynamic = "force-dynamic";
 
 export default async function CardsMazeServer() {
   let monuments: any = [];
-
   try {
-    monuments = await getMonuments();
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`
+    );
+    monuments = await res.json();
   } catch (error) {
-    console.error("Ошибка при загрузке памятников:", error);
+    console.error(error);
   }
 
   if (!monuments || monuments.length === 0) {
@@ -17,30 +20,5 @@ export default async function CardsMazeServer() {
       </div>
     );
   }
-
   return <CardsMaze monuments={monuments} />;
 }
-
-/*
-import CardsMaze from "./CardsMaze";
-
-export default async function CardsMazeServer() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`);
-
-  if (!res.ok) {
-    throw new Error(`Ошибка HTTP: ${res.status}`);
-  }
-
-  const monuments = await res.json();
-  if (!monuments || monuments.length === 0) {
-    return (
-      <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
-        Нет данных о памятниках!
-      </div>
-    );
-  }
-
-  return <CardsMaze monuments={monuments} />;
-}
-
-*/
