75445ac7a5475dde06feeb2c14f7d1f2208f2445
unknown <danil.perfiliv@gmail.com>
2025-06-24 01:06:59 +0700

    data fetch

--- src/app/components/CardsMazeServer.tsx
+++ src/app/components/CardsMazeServer.tsx
@@ -1,36 +1,46 @@
 import CardsMaze from "./CardsMaze";
+import { getMonuments } from "../../../lib/getMonuments";
 
 export default async function CardsMazeServer() {
+  let monuments: any = [];
+
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`
-    );
-
-    if (!res.ok) {
-      console.error(`Ошибка HTTP: ${res.status}`);
-      return (
-        <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
-          Временные проблемы с загрузкой данных. Пожалуйста, попробуйте позже.
-        </div>
-      );
-    }
-
-    const monuments = await res.json();
-    if (!monuments || monuments.length === 0) {
-      return (
-        <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
-          Нет данных о памятниках!
-        </div>
-      );
-    }
-
-    return <CardsMaze monuments={monuments} />;
+    monuments = await getMonuments();
   } catch (error) {
-    console.error("Ошибка при загрузке данных:", error);
+    console.error("Ошибка при загрузке памятников:", error);
+  }
+
+  if (!monuments || monuments.length === 0) {
     return (
       <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
-        Произошла ошибка при загрузке данных. Пожалуйста, обновите страницу.
+        Нет данных о памятниках!
       </div>
     );
   }
+
+  return <CardsMaze monuments={monuments} />;
 }
+
+/*
+import CardsMaze from "./CardsMaze";
+
+export default async function CardsMazeServer() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/monumentsCards`);
+
+  if (!res.ok) {
+    throw new Error(`Ошибка HTTP: ${res.status}`);
+  }
+
+  const monuments = await res.json();
+  if (!monuments || monuments.length === 0) {
+    return (
+      <div className="h-[620px] md:h-[900px] lg:h-screen flex justify-center items-center text-black text-center">
+        Нет данных о памятниках!
+      </div>
+    );
+  }
+
+  return <CardsMaze monuments={monuments} />;
+}
+
+*/
